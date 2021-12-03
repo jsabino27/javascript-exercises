a09e5b7ca5a516b6a9981e03c20a0273c935940a
Jeremy Sabino <jsabino2@go.pasadena.edu>
2021-12-03 09:56:31 -0800

    finished part 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalnum = 0;
+    for(i = 0; i <= num2; i++){
+        finalnum += i;
+    }
+    return finalnum;
 };
 
 // Do not edit below this line
