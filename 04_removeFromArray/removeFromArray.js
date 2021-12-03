22ebb2e91a953c63a4446bd0d702f4c0ef38b1db
Jeremy Sabino <jsabino2@go.pasadena.edu>
2021-12-02 22:35:24 -0800

    Testing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, num) {
+    let index = arr.indexOf(num);
+    if (index >- 1){
+        arr.splice(index, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
