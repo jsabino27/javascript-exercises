d94413f8d33dffd73ab1eb15448dd48420792be4
Jeremy Sabino <jsabino2@go.pasadena.edu>
2021-12-03 10:05:42 -0800

    finished temp

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function (ftemp) {
+  return ((ftemp - 32) * 5 / 9);
 };
 
-const ctof = function() {
-
+const ctof = function (celtemp) {
+  return (celtemp * 9/5) + 32;
 };
 
 // Do not edit below this line
