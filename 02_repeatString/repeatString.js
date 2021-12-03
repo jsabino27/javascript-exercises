bc85ec5da646ddada94c29262493b132c1e80d62
Jeremy Sabino <jsabino2@go.pasadena.edu>
2021-12-03 09:49:08 -0800

    finsihed part 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let word = "";
+    while(num > 0){
+        string += word;
+        num--;
+    }
+    return word;
 };
 
 // Do not edit below this line
